23cdd2c53b42520a9e434f6e654f26a5f824a20f
denismui <denismui5454@gmail.com>
2023-01-10 11:35:22 +0000

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Denis Mui
+
+def pow(a, b):
+	return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pyhton3
+# Author - Denis Mui
+
+def print_last_digit(number):
+	print(abs(number) % 10, end="")
+	return (abs(number) % 10)
