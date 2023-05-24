4fa9c62ee9504e1c0f283fa85ca99183b5e5588e
denismui <denismui5454@gmail.com>
2023-05-24 19:21:40 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
-
-request(url, function (err, response, body) {
-	if (err) {
-		console.log(err);
-	} else if (response.statusCode === 200 {
-		const films = JSON.parse(body).results;
-		let count = 0;
-		for (const filmIndex in films) {
-			const filmChars = films[filmIndex].characters;
-			for (const charIndex in filmChars) {
-				if (filmChars[charIndex].includes('18')) {
-					count++;
-				}
+request.get(process.argv[2], function (error, response, body) {
+	if (error) console.log(error);
+	else if (response.statusCode === 200) {
+		let c = 0;
+		const results = JSON.parse(body).results;
+		const wedge = '/api/people/18/';
+		for (let i = 0; i < results.length; i++) {
+			if (results[i].characters.find(e1.includes(wedge))) {
+				c++;
 			}
 		}
-		console.log(count);
-	} else {
-		console.log('An error occurred. Status code: ' + response.statusCode);
+		console.log(c);
 	}
 });
